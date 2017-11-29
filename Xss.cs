bc996bc64b800037163c84d8b5677852c0d527fa
Reza <mrdev1020@gmail.com>
2017-11-29 22:05:32 +0330

    Update and rename Security.cs to Xss.cs

---
+++ Xss.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MohamadReza;
+using System.ComponentModel.DataAnnotations;
+
+namespace Providers.Attributes
+{
+    public class Xss : ValidationAttribute
+    {
+        private Security _security = new Security();
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                var Txt = _security.Xss(value.ToString());
+
+                validationContext
+                    .ObjectType
+                    .GetProperty(validationContext.MemberName)
+                    .SetValue(validationContext.ObjectInstance, Txt, null);
+
+                if (String.IsNullOrEmpty(Txt))
+                    return new ValidationResult("???");
+                else
+                    return ValidationResult.Success;
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
